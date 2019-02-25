b5a76fa0aeae2d2ab79d81afb0214b2275474ad2
Arinc Elhan <arinc.elhan@nurd.com>
2019-02-25 16:21:13 +0300

    Add crawler for nba photos

---
+++ nbaphotos.py
@@ -0,0 +1,55 @@
+import urllib2, os, time
+from bs4 import BeautifulSoup
+
+base_url = 'http://www.nba.com'
+
+print 'Start crawling...'
+
+page = urllib2.urlopen(base_url + '/teams')
+soup = BeautifulSoup(page, 'html.parser')
+
+print 'Got /teams url!'
+
+teams = soup.findAll('div', attrs={'class': 'team__list'})
+
+print 'Find all teams...' + '\n'
+
+for team in teams:
+    team_url = team.find('a', href=True)
+    all_team_url = base_url + team_url['href']
+
+    team_page = urllib2.urlopen(all_team_url)
+    team_soup = BeautifulSoup(team_page, 'html.parser')
+
+    city_name = team_soup.find('p', attrs={'class': 'nba-team-header__city-name'}).text.strip()
+    team_name = team_soup.find('p', attrs={'class': 'nba-team-header__team-name'}).text.strip()
+    nba_team_name = city_name + '_' + team_name
+
+    print 'Start with ' + nba_team_name
+
+    if not os.path.exists(nba_team_name):
+        os.makedirs(nba_team_name)
+
+    os.chdir(nba_team_name)
+
+    players = team_soup.find('section', attrs={'class': 'row nba-player-index__row'})
+
+    for player in players:
+        player_name = player.find('a')['title'].lower().replace(' ', '_')
+        player_link = 'https:' + str(player.find('img')['data-src'])
+
+        print player_name
+        print player_link
+        print 'Start downloading...'
+
+        resource = urllib2.urlopen(str(player_link))
+        output = open(player_name + '.png', 'wb')
+        output.write(resource.read())
+        output.close()
+        print 'Saved image!'
+        time.sleep(1)
+
+    print 'Done with ' + nba_team_name + '\n'
+
+    os.chdir('..')
+    time.sleep(5)
